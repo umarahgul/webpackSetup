acd05f4ecec10d46554a7e709e45b9673c843d91
umarahgul <umarah.gul@gmail.com>
2023-08-17 01:45:05 +0500

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index:'./src/index.js',
+},
+
+devServer: {
+  static: './dist',
+},
+plugins: [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+],
+
+  output: {
+    filename: '[name].[contenthash].js', // Use [name].[contenthash].js 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+     
+    ],
+  },
+};
