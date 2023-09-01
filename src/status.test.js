60936572df533bbd8f98597960ceaaec26f960ac
umarahgul <umarah.gul@gmail.com>
2023-09-02 03:34:35 +0500

    Updated the status.test.js

--- src/status.test.js
+++ src/status.test.js
@@ -19,18 +19,5 @@ describe('check status update nad clear completed status', () => {
     expect(todoList[0].completed).toBe(true);
   });
 
-  test(' this clears all the tasks with status completed=true', () => {
-    todoList.length = 0;
-    const obj1 = { description: 'task1', completed: false, index: 0 };
-    const obj2 = { description: 'task2', completed: true, index: 1 };
-    // push obj2 before obj1 in this way obj2 with completed:true
-    // is removed from the list and obj1 comes at todoList[0]
-    todoList.push(obj2);
-    todoList.push(obj1);
-
-    clearCompleted(todoList);
-
-    // expect(todoList[0].completed).toBe(false);
-    expect(todoList[0].description).toBe('task1');
-  });
+  
 });
