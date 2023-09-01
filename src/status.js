8fc66f2ac6b6e145c1db5f346551dbfa6111633a
umarahgul <umarah.gul@gmail.com>
2023-09-02 03:22:59 +0500

    craeted test for status update and clearcompleetd

--- src/status.js
+++ src/status.js
@@ -17,11 +17,14 @@ export const updateStatus = (
 export const clearCompleted = (todoList) => {
   const filteredArray = todoList.filter((obj) => obj.completed === false);
   const maxIndex = filteredArray.length;
-
+ 
   for (let i = 0; i < maxIndex; i += 1) {
     filteredArray[i].index = i + 1;
+    
   }
-
+  todoList.length = 0;
+  todoList.push(...filteredArray);
+   
   localStorage.setItem('todoList', JSON.stringify(filteredArray));
 
   localStorage.setItem('todoList', JSON.stringify(filteredArray));
