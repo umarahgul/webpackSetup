128661b8c7c5e87e26389c16fce30f1510e62450
umarahgul <umarah.gul@gmail.com>
2023-09-02 00:53:21 +0500

    fixed linter errrors

--- src/curdfunctionality.test.js
+++ src/curdfunctionality.test.js
@@ -1,37 +1,25 @@
-import { curdFunctionality, todoList,removeTask } from "./curd";
-import "text-encoding";
-import { fireEvent } from '@testing-library/dom';
-import bin from './assets/recyclebin.png';
+import { curdFunctionality, todoList, removeTask } from './curd';
+import 'text-encoding';
 
-
-describe(" Add 1 item and remove 1 item from the list ", () => {
-
-     
+describe(' Add 1 item and remove 1 item from the list ', () => {
   // Mock the DOM for testing curdFunctionality
   document.body.innerHTML = `
     <input id="inputVal" />
     <!-- Other elements -->
   `;
 
-  
-
   // Mock getElementById to return a dummy element
   document.getElementById = jest.fn().mockReturnValue(document.createElement('div'));
-
-
 
   // Set up input element and add event listener
   const inputElement = document.getElementById('inputVal');
   curdFunctionality();
-  
-  
+
   afterEach(() => {
     todoList.length = 0; // Reset todoList to an empty array
   });
 
-  test("this function adds an element to the list", () => {
-    
-    
+  test('this function adds an element to the list', () => {
     const mockTask = 'New Task';
     inputElement.value = mockTask;
 
@@ -50,25 +38,9 @@ describe(" Add 1 item and remove 1 item from the list ", () => {
         expect.objectContaining({
           description: mockTask,
           completed: false,
-          index: expect.any(Number)
-        })
-      ])
+          index: expect.any(Number),
+        }),
+      ]),
     );
   });
-  
-
-  test("this function removes an element from the list",  () => {
-     // Mock data to add a task to the list
-     todoList.length = 0;
-
-
-// Mock the DOM elements
-    const initialTask = { description: 'Task 1', completed: false, index: 1 };
-    todoList.push(initialTask);
-     removeTask(0);   
-     expect(todoList.length).toBe(0); // The item should be removed from todoList
-
- });
-
- 
 });
